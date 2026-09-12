b14fee62739ca08d4198780794335751488fa192
15195606989 <15195606989@163.com>
2026-09-12 18:29:44 +0800

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,5 +1,5 @@
 local base="https://raw.githubusercontent.com/15195606989/lua/refs/heads/main/"
-local scripts={"Fly.lua","Speed.lua","ESP.lua"}
+local scripts={"Fly.lua","Speed.lua","ESP.lua","_UI.lua"}
 for _,name in ipairs(scripts)do
     local ok,err=pcall(function()
         loadstring(game:HttpGet(base..name))()
