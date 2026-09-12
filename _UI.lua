1d7939a1f83dc8a6e90bacdd34c0863b33a2ac74
15195606989 <15195606989@163.com>
2026-09-12 18:30:46 +0800

    Update _UI.lua

--- _UI.lua
+++ _UI.lua
@@ -1,17 +1,15 @@
-if not getgenv().NDS_UI then
-    getgenv().NDS_UI={
-        panels={},
-        register=function(name,hideFn,showFn)
-            getgenv().NDS_UI.panels[name]={hide=hideFn,show=showFn}
-        end,
-        showOnly=function(name)
-            for n,p in pairs(getgenv().NDS_UI.panels)do
-                if n==name then
-                    p.show()
-                else
-                    p.hide()
-                end
+getgenv().NDS_UI=getgenv().NDS_UI or {
+    panels={},
+    register=function(name,hideFn,showFn)
+        getgenv().NDS_UI.panels[name]={hide=hideFn,show=showFn}
+    end,
+    showOnly=function(name)
+        for n,p in pairs(getgenv().NDS_UI.panels)do
+            if n==name then
+                p.show()
+            else
+                p.hide()
             end
         end
-    }
-end
+    end
+}
